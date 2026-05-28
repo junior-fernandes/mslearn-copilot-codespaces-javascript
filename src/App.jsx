3ba5bbb85dba166addead8aaac380e8235cede05
Junior Fernandes <ademilsonjnr@gmail.com>
2026-05-28 18:42:52 +0000

    Alter name and style

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Junior Fernandes",
+  title: "Software Engineer",
+  email: "junior@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
